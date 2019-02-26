de787eb7fdc9735104ca310bc3df2650a3ad3805
Nermin <nermin@leap13.com>
2019-02-26 15:07:33 +0200

    the_title issue fixed

--- inc/core/common-functions.php
+++ inc/core/common-functions.php
@@ -731,7 +731,7 @@ if ( ! function_exists( 'kemet_the_post_title' ) ) {
 
 			// This will work same as `the_title` function but with Custom Title if exits.
 			if ( $echo ) {
-				printf (' $before . $title . $after ' ); // WPCS: XSS OK.
+				echo $before . $title . $after; // WPCS: XSS OK.
 			} else {
 				return $before . $title . $after;
 			}
