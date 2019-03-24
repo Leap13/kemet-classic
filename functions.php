46af3a9104f765b09e49ab1eae088d1c8175fb31
Nermin Phelopis <nermin@Nermins-iMac.local>
2019-03-24 12:52:16 +0200

    ver 0.0.3

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'KEMET_THEME_VERSION', '0.0.2' );
+define( 'KEMET_THEME_VERSION', '0.0.3' );
 define( 'KEMET_THEME_DIR', get_template_directory() . '/' );
 define( 'KEMET_THEME_URI', get_template_directory_uri() . '/' );
 define( 'KEMET_THEME_SETTINGS', 'kemet-settings' );
