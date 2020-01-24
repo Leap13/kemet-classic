9637f4d07b93e619e6b2130ebbdb79872c1b4289
AbdelRahman <abdelrahman@Remon-Mac-mini.local>
2020-01-24 16:45:51 +0200

    layout-p1

--- templates/blog/blog-layout.php
+++ templates/blog/blog-layout.php
@@ -9,13 +9,68 @@
  * @since       Kemet 1.0.0
  */
 
+$blog_post_order = kemet_get_option( 'blog-post-structure' );
+
 ?>
 <div <?php kemet_blog_layout_class( 'blog-layout-1' ); ?>>
 
 	<div class="post-content kmt-col-md-12">
+	<?php foreach($blog_post_order as $item){ ?>
+		<?php 
+		if($item == 'image'){
+			kemet_get_post_thumbnail( '<div class="kmt-blog-featured-section post-thumb kmt-col-md-12">', '</div>' ); 
+		}
+		?>
 
-		<?php kemet_blog_post_order(); ?>
+		<?php 
+		if($item == 'title-meta'){
+			do_action( 'kemet_archive_entry_header_before' ); ?> 
+			<header class="entry-header">
+				<?php
 
+					do_action( 'kemet_archive_post_title_before' );
+					
+					/* translators: 1: Current post link, 2: Current post id */
+					kemet_the_title( sprintf( '<h2 class="entry-title" itemprop="headline"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>', get_the_id() );
+
+					do_action( 'kemet_archive_post_title_after' );
+
+				?>
+				<?php
+
+					do_action( 'kemet_archive_post_meta_before' );
+
+					kemet_blog_get_post_meta();
+
+					do_action( 'kemet_archive_post_meta_after' );
+
+				?>
+			</header><!-- .entry-header -->
+		<?php } ?>
+		<?php 
+		if($item == 'content-readmore'){	
+			do_action( 'kemet_archive_entry_header_after' ); ?>
+				<div class="entry-content clear" itemprop="text">
+
+				<?php kemet_entry_content_before(); ?>
+
+				<?php kemet_the_excerpt(); ?>
+
+				<?php kemet_entry_content_after(); ?>
+
+				<?php
+					wp_link_pages(
+						array(
+							'before'      => '<div class="page-links">' . esc_html( kemet_theme_strings( 'string-blog-page-links-before', false ) ),
+							'after'       => '</div>',
+							'link_before' => '<span class="page-link">',
+							'link_after'  => '</span>',
+						)
+					);
+				?>
+			</div><!-- .entry-content .clear -->
+		<?php } ?>
+		<?php } ?>
 	</div><!-- .post-content -->
 
 </div> <!-- .blog-layout-1 -->
