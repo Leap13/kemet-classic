b59e77776a9ae7a9c1362e487d0deec347845f45
AbdelRahman <abdelrahman@Remon-Mac-mini.local>
2020-02-17 19:56:44 +0200

    smart-skin-control

--- inc/customizer/sections/colors-background/body.php
+++ inc/customizer/sections/colors-background/body.php
@@ -13,28 +13,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 $defaults = Kemet_Theme_Options::defaults();
-	/**
-	* Option: Header Layout
-	*/
+	
 	$wp_customize->add_setting(
-		KEMET_THEME_SETTINGS . '[smart-skin]', array(
-			'default'           => $defaults['smart-skin'],
+		KEMET_THEME_SETTINGS . '[smart-skin-test]', array(
+			'default'           => 'smart-skin-1',
 			'type'              => 'option',
 			'sanitize_callback' => array( 'Kemet_Customizer_Sanitizes', 'sanitize_choices' ),
 		)
 	);
 
 	$wp_customize->add_control(
-		new Kemet_Control_Radio_Image(
-			$wp_customize, KEMET_THEME_SETTINGS . '[smart-skin]', array(
+		new Kemet_Control_Smart_Skin(
+			$wp_customize, KEMET_THEME_SETTINGS . '[smart-skin-test]', array(
 				'section'  => 'section-colors-body',
 				'priority' => 1,
 				'label'    => __( 'Smart Skin', 'kemet' ),
-				'type'     => 'kmt-radio-image',
+				'type'     => 'kmt-smart-skin',
 				'choices'  => array(
 					'smart-skin-1' => array(
 						'label' => __( 'SS1', 'kemet' ),
 						'path'  => KEMET_THEME_URI . 'assets/images/ss1.png',
+						'colors' => array(
+							KEMET_THEME_SETTINGS . '[theme-color]' => '#44AAF1',
+							KEMET_THEME_SETTINGS . '[t1-color]' => '#353844',
+							KEMET_THEME_SETTINGS . '[t2-color]' => '#60626C',
+							KEMET_THEME_SETTINGS . '[b1-color]' => '#E8EAEC',
+							KEMET_THEME_SETTINGS . '[ft-color]' => '#FBFBFB',
+							KEMET_THEME_SETTINGS . '[fb-color]' => '#42434E',
+						),
 					),
 				),
 			)
