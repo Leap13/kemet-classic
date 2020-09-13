e80743f72d9990c743a05de3aeaeda0da91a42ee
nerminleap13 <nermin@leap13.com>
2020-09-13 18:36:59 +0200

    changelog-version

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 /**
  * Define Constants.
  */
-define('KEMET_THEME_VERSION', '1.1.1');
+define('KEMET_THEME_VERSION', '1.1.2');
 define('KEMET_THEME_DIR', get_template_directory().'/');
 define('KEMET_THEME_URI', get_template_directory_uri().'/');
 define('KEMET_THEME_SETTINGS', 'kemet-settings');
