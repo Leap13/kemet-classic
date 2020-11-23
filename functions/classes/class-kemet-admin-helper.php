0a34b2ea880549c71b67d715dd7572219bb821cd
AbdelRahman <abdelrahman@Remon-Mac-mini.local>
2020-11-23 18:36:18 +0200

    formate

--- functions/classes/class-kemet-admin-helper.php
+++ functions/classes/class-kemet-admin-helper.php
@@ -11,73 +11,75 @@
  * @since       Kemet 1.0.0
  */
 
-if ( ! class_exists( 'Kemet_Admin_Helper' ) ) :
+if (! class_exists('Kemet_Admin_Helper')) :
 
-	/**
-	 * Admin Helper
-	 */
-	final class Kemet_Admin_Helper {
+    /**
+     * Admin Helper
+     */
+    final class Kemet_Admin_Helper
+    {
 
-		/**
-		 * Returns an option from the database for
-		 * the admin settings page.
-		 *
-		 * @param  string  $key     The option key.
-		 * @param  boolean $network Whether to allow the network admin setting to be overridden on subsites.
-		 * @return string           Return the option value
-		 */
-		static public function get_admin_settings_option( $key, $network = false ) {
+        /**
+         * Returns an option from the database for
+         * the admin settings page.
+         *
+         * @param  string  $key     The option key.
+         * @param  boolean $network Whether to allow the network admin setting to be overridden on subsites.
+         * @return string           Return the option value
+         */
+        public static function get_admin_settings_option($key, $network = false)
+        {
 
-			// Get the site-wide option if we're in the network admin.
-			if ( $network && is_multisite() ) {
-				$value = get_site_option( $key );
-			} else {
-				$value = get_option( $key );
-			}
+            // Get the site-wide option if we're in the network admin.
+            if ($network && is_multisite()) {
+                $value = get_site_option($key);
+            } else {
+                $value = get_option($key);
+            }
 
-			return $value;
-		}
+            return $value;
+        }
 
-		/**
-		 * Updates an option from the admin settings page.
-		 *
-		 * @param string $key       The option key.
-		 * @param mixed  $value     The value to update.
-		 * @param bool   $network   Whether to allow the network admin setting to be overridden on subsites.
-		 * @return mixed
-		 */
-		static public function update_admin_settings_option( $key, $value, $network = false ) {
+        /**
+         * Updates an option from the admin settings page.
+         *
+         * @param string $key       The option key.
+         * @param mixed  $value     The value to update.
+         * @param bool   $network   Whether to allow the network admin setting to be overridden on subsites.
+         * @return mixed
+         */
+        public static function update_admin_settings_option($key, $value, $network = false)
+        {
 
-			// Update the site-wide option since we're in the network admin.
-			if ( $network && is_multisite() ) {
-				update_site_option( $key, $value );
-			} else {
-				update_option( $key, $value );
-			}
+            // Update the site-wide option since we're in the network admin.
+            if ($network && is_multisite()) {
+                update_site_option($key, $value);
+            } else {
+                update_option($key, $value);
+            }
+        }
 
-		}
+        /**
+         * Returns an option from the database for
+         * the admin settings page.
+         *
+         * @param string $key The option key.
+         * @param bool   $network Whether to allow the network admin setting to be overridden on subsites.
+         * @return mixed
+         */
+        public static function delete_admin_settings_option($key, $network = false)
+        {
 
-		/**
-		 * Returns an option from the database for
-		 * the admin settings page.
-		 *
-		 * @param string $key The option key.
-		 * @param bool   $network Whether to allow the network admin setting to be overridden on subsites.
-		 * @return mixed
-		 */
-		static public function delete_admin_settings_option( $key, $network = false ) {
+            // Get the site-wide option if we're in the network admin.
+            if ($network && is_multisite()) {
+                $value = delete_site_option($key);
+            } else {
+                $value = delete_option($key);
+            }
 
-			// Get the site-wide option if we're in the network admin.
-			if ( $network && is_multisite() ) {
-				$value = delete_site_option( $key );
-			} else {
-				$value = delete_option( $key );
-			}
-
-			return $value;
-		}
-
-	}
+            return $value;
+        }
+    }
 
 
 endif;
