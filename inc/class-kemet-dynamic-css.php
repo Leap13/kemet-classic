26b9c7a47fb136f383b7771b13ae5fed9019c277
Abdelrhman Haridy <a.abdelhamed@leap13.com>
2021-03-01 16:52:18 +0200

    contact7 from colors

--- inc/class-kemet-dynamic-css.php
+++ inc/class-kemet-dynamic-css.php
@@ -477,7 +477,7 @@ if (!class_exists('Kemet_Dynamic_CSS')) {
                     'color' => esc_attr($text_meta_color),
                 ),
                 //Input
-                'input,input[type="text"],input[type="email"],input[type="url"],input[type="password"],input[type="reset"],input[type="search"], textarea , select,.widget_search .search-field, .widget_search .search-field:focus' => array(
+                'input,input[type="text"],input[type="email"],input[type="url"],input[type="password"],input[type="reset"],input[type="search"], textarea , select,.widget_search .search-field, .widget_search .search-field:focus,.kmt-single-post .wpcf7 input:not([type=submit]),.kmt-single-post .wpcf7 input:not([type=submit]):focus,.kmt-single-post .wpcf7 select:focus,.kmt-single-post .wpcf7 textarea:focus' => array(
                     'color' => esc_attr($text_meta_color),
                     'background-color' => esc_attr($input_bg_color),
                     'border-color' => esc_attr($input_border_color),
@@ -714,7 +714,7 @@ if (!class_exists('Kemet_Dynamic_CSS')) {
                     'background-color' => esc_attr($kemet_footer_link_color),
                 ),
 
-                '.kemet-footer input,.kemet-footer input[type="text"],.kemet-footer input[type="email"],.kemet-footer input[type="url"],.kemet-footer input[type="password"],.kemet-footer input[type="reset"],.kemet-footer input[type="search"],.kemet-footer textarea' => array(
+                '.kemet-footer input,.kemet-footer input[type="text"],.kemet-footer input[type="email"],.kemet-footer input[type="url"],.kemet-footer input[type="password"],.kemet-footer input[type="reset"],.kemet-footer input[type="search"],.kemet-footer textarea,.kmt-single-post .kemet-footer .wpcf7 input:not([type=submit]),.kmt-single-post .kemet-footer .wpcf7 input:not([type=submit]):focus,.kmt-single-post .kemet-footer .wpcf7 select:focus,.kmt-single-post .kemet-footer .wpcf7 textarea:focus' => array(
                     'color' => esc_attr($footer_input_color),
                     'background' => esc_attr($footer_input_bg_color),
                     'border-color' => esc_attr($footer_input_border_color),
