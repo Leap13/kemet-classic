884d14a5f24f055ffaed6627faae64319ba6b2d4
Abdelrhman Haridy <a.abdelhamed@leap13.com>
2021-04-05 18:56:57 +0200

    section-container.php

--- inc/compatibility/woocommerce/customizer/sections/section-container.php
+++ inc/compatibility/woocommerce/customizer/sections/section-container.php
@@ -14,28 +14,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 
-	/**
-	 * Option: Shop Page
-	 */
-	$wp_customize->add_setting(
-		KEMET_THEME_SETTINGS . '[woocommerce-content-layout]', array(
-			'default'           => kemet_get_option( 'woocommerce-content-layout' ),
-			'type'              => 'option',
-			'sanitize_callback' => array( 'Kemet_Customizer_Sanitizes', 'sanitize_choices' ),
-		)
-	);
-	$wp_customize->add_control(
-		KEMET_THEME_SETTINGS . '[woocommerce-content-layout]', array(
-			'type'     => 'select',
-			'section'  => 'section-container-layout',
-			'priority' => 85,
-			'label'    => __( 'Container for WooCommerce', 'kemet' ),
-			'choices'  => array(
-				'default'                 => __( 'Default', 'kemet' ),
-				'boxed-container'         => __( 'Boxed', 'kemet' ),
-				'content-boxed-container' => __( 'Content Boxed', 'kemet' ),
-				'plain-container'         => __( 'Full Width / Contained', 'kemet' ),
-				'page-builder'            => __( 'Full Width / Stretched', 'kemet' ),
-			),
-		)
-	);
+/**
+ * Option: Shop Page
+ */
+$wp_customize->add_setting(
+	KEMET_THEME_SETTINGS . '[woocommerce-content-layout]',
+	array(
+		'default'           => kemet_get_option( 'woocommerce-content-layout' ),
+		'type'              => 'option',
+		'sanitize_callback' => array( 'Kemet_Customizer_Sanitizes', 'sanitize_choices' ),
+	)
+);
+$wp_customize->add_control(
+	KEMET_THEME_SETTINGS . '[woocommerce-content-layout]',
+	array(
+		'type'     => 'select',
+		'section'  => 'section-container-layout',
+		'priority' => 85,
+		'label'    => __( 'Container for WooCommerce', 'kemet' ),
+		'choices'  => array(
+			'default'                 => __( 'Default', 'kemet' ),
+			'boxed-container'         => __( 'Boxed', 'kemet' ),
+			'content-boxed-container' => __( 'Content Boxed', 'kemet' ),
+			'plain-container'         => __( 'Full Width / Contained', 'kemet' ),
+			'page-builder'            => __( 'Full Width / Stretched', 'kemet' ),
+		),
+	)
+);
