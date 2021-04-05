6ceec615b562345b84439692d7049e85725ddcc7
Abdelrhman Haridy <a.abdelhamed@leap13.com>
2021-04-05 18:55:37 +0200

    woo-shop-single.php

--- inc/compatibility/woocommerce/customizer/sections/layout/woo-shop-single.php
+++ inc/compatibility/woocommerce/customizer/sections/layout/woo-shop-single.php
@@ -13,21 +13,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-	/**
-	 * Option: Disable Breadcrumb
-	 */
-	$wp_customize->add_setting(
-		KEMET_THEME_SETTINGS . '[single-product-breadcrumb-disable]', array(
-			'default'           => kemet_get_option( 'single-product-breadcrumb-disable' ),
-			'type'              => 'option',
-			'sanitize_callback' => array( 'Kemet_Customizer_Sanitizes', 'sanitize_checkbox' ),
-		)
-	);
-	$wp_customize->add_control(
-		KEMET_THEME_SETTINGS . '[single-product-breadcrumb-disable]', array(
-			'section'  => 'section-woo-shop-single',
-			'label'    => __( 'Disable Breadcrumb', 'kemet' ),
-			'priority' => 10,
-			'type'     => 'checkbox',
-		)
-	);
+/**
+ * Option: Disable Breadcrumb
+ */
+$wp_customize->add_setting(
+	KEMET_THEME_SETTINGS . '[single-product-breadcrumb-disable]',
+	array(
+		'default'           => kemet_get_option( 'single-product-breadcrumb-disable' ),
+		'type'              => 'option',
+		'sanitize_callback' => array( 'Kemet_Customizer_Sanitizes', 'sanitize_checkbox' ),
+	)
+);
+$wp_customize->add_control(
+	KEMET_THEME_SETTINGS . '[single-product-breadcrumb-disable]',
+	array(
+		'section'  => 'section-woo-shop-single',
+		'label'    => __( 'Disable Breadcrumb', 'kemet' ),
+		'priority' => 10,
+		'type'     => 'checkbox',
+	)
+);
