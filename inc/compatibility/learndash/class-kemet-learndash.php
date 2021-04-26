3c58ff8ef9bda0282b0e63224bba609cf6a7b610
aaaharidy <a.abdelhamed@leap13.com>
2021-04-27 01:48:02 +0200

    kemet buttons style with learndash

--- inc/compatibility/learndash/class-kemet-learndash.php
+++ inc/compatibility/learndash/class-kemet-learndash.php
@@ -132,30 +132,32 @@ if ( ! class_exists( 'Kemet_LearnDash' ) ) :
 		 */
 		public function dynamic_css( $dynamic_css ) {
 
-			$is_site_rtl  = is_rtl();
-			$link_color   = kemet_get_option( 'link-color' );
-			$theme_color  = kemet_get_option( 'theme-color' );
-			$text_color   = kemet_get_option( 'text-color' );
-			$link_h_color = kemet_get_option( 'link-h-color' );
+			$is_site_rtl     = is_rtl();
+			$link_color      = kemet_get_option( 'content-link-color' );
+			$theme_color     = kemet_get_option( 'theme-color' );
+			$text_meta_color = kemet_get_option( 'text-meta-color' );
+			$link_h_color    = kemet_get_option( 'content-link-h-color' );
 
 			$body_font_family = kemet_body_font_family();
 
-			$link_forground_color  = kemet_get_foreground_color( $link_color );
-			$theme_forground_color = kemet_get_foreground_color( $theme_color );
-			$btn_color             = kemet_get_option( 'button-color' );
-			if ( empty( $btn_color ) ) {
-				$btn_color = $link_forground_color;
-			}
-
-			$btn_h_color = kemet_get_option( 'button-h-color' );
-			if ( empty( $btn_h_color ) ) {
-				$btn_h_color = kemet_get_foreground_color( $link_h_color );
-			}
-			$btn_bg_color   = kemet_get_option( 'button-bg-color', '', $theme_color );
-			$btn_bg_h_color = kemet_get_option( 'button-bg-h-color', '', $link_h_color );
-
-			$btn_border_radius = kemet_get_option( 'button-radius' );
-
+			$link_forground_color         = kemet_get_foreground_color( $link_color );
+			$theme_forground_color        = kemet_get_foreground_color( $theme_color );
+			$btn_text_color               = kemet_get_option( 'button-color', '#ffffff' );
+			$btn_font_size                = kemet_get_option( 'buttons-font-size' );
+			$btn_font_family              = kemet_get_option( 'buttons-font-family' );
+			$btn_font_weight              = kemet_get_option( 'buttons-font-weight' );
+			$btn_text_tranform            = kemet_get_option( 'buttons-text-transform' );
+			$btn_font_style               = kemet_get_option( 'buttons-font-style' );
+			$btn_line_height              = kemet_get_option( 'buttons-line-height' );
+			$btn_letter_spacing           = kemet_get_option( 'buttons-letter-spacing' );
+			$btn_border_radius            = kemet_get_option( 'button-radius' );
+			$btn_padding                  = kemet_get_option( 'button-spacing' );
+			$btn_text_hover_color         = kemet_get_option( 'button-h-color' );
+			$btn_bg_color                 = kemet_get_option( 'button-bg-color', $theme_color );
+			$btn_border_size              = kemet_get_option( 'btn-border-size' );
+			$btn_border_color             = kemet_get_option( 'btn-border-color' );
+			$btn_border_h_color           = kemet_get_option( 'btn-border-h-color' );
+			$btn_bg_hover_color           = kemet_get_option( 'button-bg-h-color', kemet_color_brightness( $theme_color, 0.8, 'dark' ) );
 			$archive_post_title_font_size = kemet_get_option( 'font-size-page-title' );
 
 			$css_output = array(
@@ -163,15 +165,28 @@ if ( ! class_exists( 'Kemet_LearnDash' ) ) :
 					'font-family' => kemet_get_font_family( $body_font_family ),
 				),
 				'body #ld_course_list .btn, body a.btn-blue, body a.btn-blue:visited, body a#quiz_continue_link, body .btn-join, body .learndash_checkout_buttons input.btn-join[type="button"], body #btn-join, body .learndash_checkout_buttons input.btn-join[type="submit"], body .wpProQuiz_content .wpProQuiz_button2' => array(
-					'color'            => $btn_color,
-					'border-color'     => $btn_bg_color,
-					'background-color' => $btn_bg_color,
-					'border-radius'    => kemet_get_css_value( $btn_border_radius, 'px' ),
+					'border-radius'    => kemet_responsive_slider( $btn_border_radius, 'desktop' ),
+					'color'            => esc_attr( $btn_text_color ),
+					'background-color' => esc_attr( $btn_bg_color ),
+					'border'           => 'solid',
+					'border-color'     => esc_attr( $btn_border_color ),
+					'border-width'     => kemet_get_css_value( $btn_border_size, 'px' ),
+					'padding-top'      => kemet_responsive_spacing( $btn_padding, 'top', 'desktop' ),
+					'padding-bottom'   => kemet_responsive_spacing( $btn_padding, 'bottom', 'desktop' ),
+					'padding-right'    => kemet_responsive_spacing( $btn_padding, 'right', 'desktop' ),
+					'padding-left'     => kemet_responsive_spacing( $btn_padding, 'left', 'desktop' ),
+					'font-size'        => kemet_responsive_slider( $btn_font_size, 'desktop' ),
+					'font-family'      => kemet_get_font_family( $btn_font_family ),
+					'font-weight'      => esc_attr( $btn_font_weight ),
+					'text-transform'   => esc_attr( $btn_text_tranform ),
+					'font-style'       => esc_attr( $btn_font_style ),
+					'line-height'      => kemet_responsive_slider( $btn_line_height, 'desktop' ),
+					'letter-spacing'   => kemet_responsive_slider( $btn_letter_spacing, 'desktop' ),
 				),
 				'body #ld_course_list .btn:hover, body #ld_course_list .btn:focus, body a.btn-blue:hover, body a.btn-blue:focus, body a#quiz_continue_link:hover, body a#quiz_continue_link:focus, body .btn-join:hover, body .learndash_checkout_buttons input.btn-join[type="button"]:hover, body .btn-join:focus, body .learndash_checkout_buttons input.btn-join[type="button"]:focus, .wpProQuiz_content .wpProQuiz_button2:hover, .wpProQuiz_content .wpProQuiz_button2:focus, body #btn-join:hover, body .learndash_checkout_buttons input.btn-join[type="submit"]:hover, body #btn-join:focus, body .learndash_checkout_buttons input.btn-join[type="submit"]:focus' => array(
-					'color'            => $btn_h_color,
-					'border-color'     => $btn_bg_h_color,
-					'background-color' => $btn_bg_h_color,
+					'color'            => esc_attr( $btn_text_hover_color ),
+					'border-color'     => esc_attr( $btn_border_h_color ),
+					'background-color' => esc_attr( $btn_bg_hover_color ),
 				),
 				'body dd.course_progress div.course_progress_blue, body .wpProQuiz_content .wpProQuiz_time_limit .wpProQuiz_progress' => array(
 					'background-color' => $theme_color,
@@ -180,7 +195,7 @@ if ( ! class_exists( 'Kemet_LearnDash' ) ) :
 					'color' => $link_color,
 				),
 				'.learndash .notcompleted:before, #learndash_profile .notcompleted:before, .learndash_topic_dots ul .topic-notcompleted span:before, .learndash_navigation_lesson_topics_list .topic-notcompleted span:before, .learndash_navigation_lesson_topics_list ul .topic-notcompleted span:before, .learndash .topic-notcompleted span:before' => array(
-					'color' => kemet_hex_to_rgba( $text_color, .5 ),
+					'color' => kemet_hex_to_rgba( $text_meta_color, .5 ),
 				),
 				'body .thumbnail.course .ld_course_grid_price, body .thumbnail.course .ld_course_grid_price.ribbon-enrolled, body #learndash_lessons #lesson_heading, body #learndash_profile .learndash_profile_heading, body #learndash_quizzes #quiz_heading, body #learndash_lesson_topics_list div > strong, body .learndash-pager span a, body #learndash_profile .learndash_profile_quiz_heading' => array(
 					'background-color' => $theme_color,
@@ -202,27 +217,26 @@ if ( ! class_exists( 'Kemet_LearnDash' ) ) :
 				),
 			);
 
-			if ( ! kemet_get_option( 'learndash-lesson-serial-number' ) ) {
-				$css_output['body #course_list .list-count, body #lessons_list .list-count, body #quiz_list .list-count'] = array(
-					'display' => 'none',
-				);
-				$css_output['body #course_list > div h4 > a, body #lessons_list > div h4 > a, body #quiz_list > div h4 > a, body #learndash_course_content .learndash_topic_dots ul > li a'] = array(
-					'padding-left' => '.75em',
-					'margin-left'  => 'auto',
-				);
-			}
-			if ( ! kemet_get_option( 'learndash-differentiate-rows' ) ) {
-				$css_output['body #course_list > div:nth-of-type(odd), body #lessons_list > div:nth-of-type(odd), body #quiz_list > div:nth-of-type(odd), body #learndash_lesson_topics_list .learndash_topic_dots ul > li.nth-of-type-odd'] = array(
-					'background' => 'none',
-				);
-			}
-
 			/* Parse CSS from array() */
 			$css_output = kemet_parse_css( $css_output );
 
 			$tablet_typography = array(
 				'#ld_course_list .entry-title' => array(
 					'font-size' => kemet_responsive_font( $archive_post_title_font_size, 'tablet', 30 ),
+				),
+				'body #learndash_lessons a, body #learndash_quizzes a, body .expand_collapse a, body .learndash_topic_dots a, body .learndash_topic_dots a > span, body #learndash_lesson_topics_list span a, body #learndash_profile a, body #learndash_profile a span' => array(
+					'font-family' => kemet_get_font_family( $body_font_family ),
+				),
+				'body #ld_course_list .btn, body a.btn-blue, body a.btn-blue:visited, body a#quiz_continue_link, body .btn-join, body .learndash_checkout_buttons input.btn-join[type="button"], body #btn-join, body .learndash_checkout_buttons input.btn-join[type="submit"], body .wpProQuiz_content .wpProQuiz_button2' => array(
+					'border-radius'  => kemet_responsive_slider( $btn_border_radius, 'tablet' ),
+					'border-width'   => kemet_get_css_value( $btn_border_size, 'px' ),
+					'padding-top'    => kemet_responsive_spacing( $btn_padding, 'top', 'tablet' ),
+					'padding-bottom' => kemet_responsive_spacing( $btn_padding, 'bottom', 'tablet' ),
+					'padding-right'  => kemet_responsive_spacing( $btn_padding, 'right', 'tablet' ),
+					'padding-left'   => kemet_responsive_spacing( $btn_padding, 'left', 'tablet' ),
+					'font-size'      => kemet_responsive_slider( $btn_font_size, 'tablet' ),
+					'line-height'    => kemet_responsive_slider( $btn_line_height, 'tablet' ),
+					'letter-spacing' => kemet_responsive_slider( $btn_letter_spacing, 'tablet' ),
 				),
 			);
 			/* Parse CSS from array()*/
@@ -262,13 +276,24 @@ if ( ! class_exists( 'Kemet_LearnDash' ) ) :
 				'#ld_course_info_mycourses_list .ld-course-info-my-courses .ld-entry-title' => array(
 					'margin' => '0 0 20px',
 				),
+				'body #ld_course_list .btn, body a.btn-blue, body a.btn-blue:visited, body a#quiz_continue_link, body .btn-join, body .learndash_checkout_buttons input.btn-join[type="button"], body #btn-join, body .learndash_checkout_buttons input.btn-join[type="submit"], body .wpProQuiz_content .wpProQuiz_button2' => array(
+					'border-radius'  => kemet_responsive_slider( $btn_border_radius, 'mobile' ),
+					'border-width'   => kemet_get_css_value( $btn_border_size, 'px' ),
+					'padding-top'    => kemet_responsive_spacing( $btn_padding, 'top', 'mobile' ),
+					'padding-bottom' => kemet_responsive_spacing( $btn_padding, 'bottom', 'mobile' ),
+					'padding-right'  => kemet_responsive_spacing( $btn_padding, 'right', 'mobile' ),
+					'padding-left'   => kemet_responsive_spacing( $btn_padding, 'left', 'mobile' ),
+					'font-size'      => kemet_responsive_slider( $btn_font_size, 'mobile' ),
+					'line-height'    => kemet_responsive_slider( $btn_line_height, 'mobile' ),
+					'letter-spacing' => kemet_responsive_slider( $btn_letter_spacing, 'mobile' ),
+				),
 			);
 
 			/* Parse CSS from array() -> max-width: (mobile-breakpoint) px */
 			$css_output .= kemet_parse_css( $mobile_typography, '', kemet_get_mobile_breakpoint() );
 
 			if ( $is_site_rtl ) {
-				$mobile_typography_lang_direction_css = array(
+				$mobile_typography_direction_css = array(
 					'#ld_course_info_mycourses_list .ld-course-info-my-courses img' => array(
 						'display'      => 'block',
 						'margin-right' => 'initial',
@@ -277,7 +302,7 @@ if ( ! class_exists( 'Kemet_LearnDash' ) ) :
 					),
 				);
 			} else {
-				$mobile_typography_lang_direction_css = array(
+				$mobile_typography_direction_css = array(
 					'#ld_course_info_mycourses_list .ld-course-info-my-courses img' => array(
 						'display'     => 'block',
 						'margin-left' => 'initial',
@@ -288,7 +313,7 @@ if ( ! class_exists( 'Kemet_LearnDash' ) ) :
 			}
 
 			/* Parse CSS from array() -> max-width: (mobile-breakpoint) px */
-			$css_output .= kemet_parse_css( $mobile_typography_lang_direction_css, '', kemet_get_mobile_breakpoint() );
+			$css_output .= kemet_parse_css( $mobile_typography_direction_css, '', kemet_get_mobile_breakpoint() );
 
 			$dynamic_css .= apply_filters( 'kemet_theme_learndash_dynamic_css', $css_output );
 
