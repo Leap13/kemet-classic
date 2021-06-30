8bb697bd4a47b9c5418c4317272614f2125caa0b
aaaharidy <a.abdelhamed@leap13.com>
2021-06-30 16:44:38 +0200

    build index

--- inc/customizer/custom-controls/react/build/index.asset.php
+++ inc/customizer/custom-controls/react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '04c1eaf3e36c51776ea62c0c65d2e0f3');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '120433a67e9c4e56d910cf557b08f91e');
