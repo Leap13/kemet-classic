0ca38f067691cb942d8417052481224a417d8bc3
salmamohamed74 <salma.mohamed@leap13.com>
2021-06-30 15:49:55 +0200

    delete console.log statement

--- inc/customizer/custom-controls/react/build/index.asset.php
+++ inc/customizer/custom-controls/react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '13f1eb30b2d80d3972dec8f575f76d39');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '04c1eaf3e36c51776ea62c0c65d2e0f3');
