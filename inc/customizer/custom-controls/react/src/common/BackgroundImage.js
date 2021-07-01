799dde84a546b77607a950315561e19614310832
salmamohamed74 <salma.mohamed@leap13.com>
2021-07-01 15:58:22 +0200

    create filrs ja to mange background component

---
+++ inc/customizer/custom-controls/react/src/common/BackgroundImage.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Dashicon, Button, ColorIndicator, TabPanel, __experimentalGradientPicker, SelectControl, ColorPalette } from '@wordpress/components';
+
+import PatternPicker from './pattern';
+import ImagePicker from './ImagePicker';
+
+export default function BackgroundImage(props) {
+    return (
+        <div className={`kmt-image-background-wrap`}>
+            <TabPanel className="kemet-popover-tabs kemet-background-tabs"
+                activeClass="active-tab"
+                initialTabName={'image'}
+                tabs={[{
+                    name: 'image',
+                    title: __('Image'),
+                    className: 'kemet-color-background',
+                },
+                {
+                    name: 'pattern',
+                    title: __('Pattern'),
+                    className: 'kemet-color-background',
+                }]}>
+                {
+                    (tab) => {
+                        let tabout;
+
+                        if (tab.name) {
+                            if ('image' === tab.name) {
+                                tabout = (
+
+                                    <ImagePicker
+                                        open={props.open}
+                                        media={props.media}
+                                        backgroundAttachment={props.backgroundAttachment}
+                                        backgroundRepeat={props.backgroundRepeat}
+                                        backgroundSize={props.backgroundSize}
+                                        backgroundImage={props.backgroundImage}
+                                        onChangeImageOption={props.onChangeImageOption}
+                                        onSelectImage={props.onSelectImage}
+                                        onRemoveImage={props.onRemoveImage}
+                                    />
+
+                                )
+                            }
+                            else {
+                                tabout = (
+                                    <>
+                                        <PatternPicker
+                                            onSelectImage={props.onSelectImage}
+                                        />
+                                    </>
+                                )
+                            }
+                        }
+                        return <div>{tabout}</div>;
+                    }
+                }
+
+            </TabPanel>
+
+        </div>
+    )
+}
