b74f36a88b2895a2f4450758238afa864da12a96
salmamohamed74 <salma.mohamed@leap13.com>
2021-07-01 16:37:11 +0200

    add function to mange responsive group-color

--- inc/customizer/custom-controls/react/build/index.asset.php
+++ inc/customizer/custom-controls/react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '8e8db11f8801ee2ea29e43a17cc847be');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '83f73171e6002e12fbc23619c8134f6f');
--- inc/customizer/custom-controls/react/build/index.js
+++ inc/customizer/custom-controls/react/build/index.js
@@ -24492,7 +24492,7 @@ var KemetColorPicker = function KemetColorPicker(_ref) {
 /*!*****************************************!*\
   !*** ./src/common/responsive-helper.js ***!
   \*****************************************/
-/*! exports provided: kemetGetResponsiveSliderJs, kemetGetResponsiveSpacingJs, kemetGetResponsiveIconJs, kemetGetResponsiveColorJs */
+/*! exports provided: kemetGetResponsiveSliderJs, kemetGetResponsiveSpacingJs, kemetGetResponsiveIconJs, kemetGetResponsiveColorJs, kemetGetResponsiveBgJs, KemetGetResponsiveColorGroupJs */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -24501,128 +24501,213 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "kemetGetResponsiveSpacingJs", function() { return kemetGetResponsiveSpacingJs; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "kemetGetResponsiveIconJs", function() { return kemetGetResponsiveIconJs; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "kemetGetResponsiveColorJs", function() { return kemetGetResponsiveColorJs; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "kemetGetResponsiveBgJs", function() { return kemetGetResponsiveBgJs; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "KemetGetResponsiveColorGroupJs", function() { return KemetGetResponsiveColorGroupJs; });
 function kemetGetResponsiveSliderJs(control) {
-  "use strict";
+  'use strict';
 
-  var device = jQuery(".wp-full-overlay-footer .devices button.active").attr("data-device");
-  jQuery(".wrapper .input-field-wrapper").removeClass("active");
-  jQuery(".wrapper .input-field-wrapper." + device).addClass("active");
-  jQuery(".wrapper .kmt-responsive-control-btns li").removeClass("active");
-  jQuery(".wrapper   .kmt-responsive-control-btns li." + device).addClass("active");
-  jQuery(".wp-full-overlay-footer .devices button").on("click", function () {
-    var device = jQuery(this).attr("data-device");
-    jQuery(".customize-control-kmt-responsive-slider .input-field-wrapper, .customize-control .kmt-responsive-slider-btns > li").removeClass("active");
-    jQuery(".customize-control-kmt-responsive-slider .input-field-wrapper." + device + ", .customize-control .kmt-responsive-slider-btns > li." + device).addClass("active");
+  var device = jQuery('.wp-full-overlay-footer .devices button.active').attr('data-device');
+  jQuery('.wrapper .input-field-wrapper').removeClass('active');
+  jQuery('.wrapper .input-field-wrapper.' + device).addClass('active');
+  jQuery('.wrapper .kmt-responsive-control-btns li').removeClass('active');
+  jQuery('.wrapper   .kmt-responsive-control-btns li.' + device).addClass('active');
+  jQuery('.wp-full-overlay-footer .devices button').on('click', function () {
+    var device = jQuery(this).attr('data-device');
+    jQuery('.customize-control-kmt-responsive-slider .input-field-wrapper, .customize-control .kmt-responsive-slider-btns > li').removeClass('active');
+    jQuery('.customize-control-kmt-responsive-slider .input-field-wrapper.' + device + ', .customize-control .kmt-responsive-slider-btns > li.' + device).addClass('active');
   });
-  control.container.find(".kmt-responsive-control-btns button i").on("click", function (event) {
+  control.container.find('.kmt-responsive-control-btns button i').on('click', function (event) {
     event.preventDefault();
-    var device = jQuery(this).parent("button").attr("data-device");
+    var device = jQuery(this).parent('button').attr('data-device');
 
-    if ("desktop" == device) {
-      device = "tablet";
-    } else if ("tablet" == device) {
-      device = "mobile";
+    if ('desktop' == device) {
+      device = 'tablet';
+    } else if ('tablet' == device) {
+      device = 'mobile';
     } else {
-      device = "desktop";
+      device = 'desktop';
     }
 
-    jQuery('.wp-full-overlay-footer .devices button[data-device="' + device + '"]').trigger("click");
+    jQuery('.wp-full-overlay-footer .devices button[data-device="' + device + '"]').trigger('click');
   });
 }
 function kemetGetResponsiveSpacingJs(control) {
-  "use strict";
+  'use strict';
 
-  var device = jQuery(".wp-full-overlay-footer .devices button.active").attr("data-device");
-  jQuery(".customize-control-kmt-responsive-spacing .input-wrapper .kmt-spacing-wrapper").removeClass("active");
-  jQuery(".customize-control-kmt-responsive-spacing .input-wrapper .kmt-spacing-wrapper." + device).addClass("active");
-  jQuery(".customize-control .kmt-spacing-responsive-btns li").removeClass("active");
-  jQuery(".customize-control .kmt-spacing-responsive-btns li." + device).addClass("active");
-  jQuery(".wp-full-overlay-footer .devices button").on("click", function () {
-    var device = jQuery(this).attr("data-device");
-    jQuery(".customize-control-kmt-responsive-spacing .input-wrapper .kmt-spacing-wrapper, .customize-control .kmt-spacing-responsive-btns > li").removeClass("active");
-    jQuery(".customize-control-kmt-responsive-spacing .input-wrapper .kmt-spacing-wrapper." + device + ", .customize-control .kmt-spacing-responsive-btns > li." + device).addClass("active");
+  var device = jQuery('.wp-full-overlay-footer .devices button.active').attr('data-device');
+  jQuery('.customize-control-kmt-responsive-spacing .input-wrapper .kmt-spacing-wrapper').removeClass('active');
+  jQuery('.customize-control-kmt-responsive-spacing .input-wrapper .kmt-spacing-wrapper.' + device).addClass('active');
+  jQuery('.customize-control .kmt-spacing-responsive-btns li').removeClass('active');
+  jQuery('.customize-control .kmt-spacing-responsive-btns li.' + device).addClass('active');
+  jQuery('.wp-full-overlay-footer .devices button').on('click', function () {
+    var device = jQuery(this).attr('data-device');
+    jQuery('.customize-control-kmt-responsive-spacing .input-wrapper .kmt-spacing-wrapper, .customize-control .kmt-spacing-responsive-btns > li').removeClass('active');
+    jQuery('.customize-control-kmt-responsive-spacing .input-wrapper .kmt-spacing-wrapper.' + device + ', .customize-control .kmt-spacing-responsive-btns > li.' + device).addClass('active');
   });
-  control.container.find(".kmt-spacing-responsive-btns button").on("click", function (event) {
+  control.container.find('.kmt-spacing-responsive-btns button').on('click', function (event) {
     event.preventDefault();
-    var device = jQuery(this).attr("data-device");
+    var device = jQuery(this).attr('data-device');
 
-    if ("desktop" == device) {
-      device = "tablet";
-    } else if ("tablet" == device) {
-      device = "mobile";
+    if ('desktop' == device) {
+      device = 'tablet';
+    } else if ('tablet' == device) {
+      device = 'mobile';
     } else {
-      device = "desktop";
+      device = 'desktop';
     }
 
-    jQuery('.wp-full-overlay-footer .devices button[data-device="' + device + '"]').trigger("click");
+    jQuery('.wp-full-overlay-footer .devices button[data-device="' + device + '"]').trigger('click');
   });
 }
 function kemetGetResponsiveIconJs(control) {
-  "use strict";
+  'use strict';
 
-  var device = jQuery(".wp-full-overlay-footer .devices button.active").attr("data-device");
-  jQuery(".customize-control-kmt-responsive-icon-select .responsive-icon-select").removeClass("active");
-  jQuery(".customize-control-kmt-responsive-icon-select .responsive-icon-select." + device).addClass("active");
-  jQuery(".customize-control .kmt-responsive-icon-select-btns li").removeClass("active");
-  jQuery(".customize-control .kmt-responsive-icon-select-btns li." + device).addClass("active");
-  jQuery(".wp-full-overlay-footer .devices button").on("click", function () {
-    var device = jQuery(this).attr("data-device");
-    jQuery(".customize-control-kmt-responsive-icon-select .responsive-icon-select, .customize-control .kmt-spacing-responsive-btns > li").removeClass("active");
-    jQuery(".customize-control-kmt-responsive-icon-select .responsive-icon-select." + device + ", .customize-control .kmt-spacing-responsive-btns > li." + device).addClass("active");
+  var device = jQuery('.wp-full-overlay-footer .devices button.active').attr('data-device');
+  jQuery('.customize-control-kmt-responsive-icon-select .responsive-icon-select').removeClass('active');
+  jQuery('.customize-control-kmt-responsive-icon-select .responsive-icon-select.' + device).addClass('active');
+  jQuery('.customize-control .kmt-responsive-icon-select-btns li').removeClass('active');
+  jQuery('.customize-control .kmt-responsive-icon-select-btns li.' + device).addClass('active');
+  jQuery('.wp-full-overlay-footer .devices button').on('click', function () {
+    var device = jQuery(this).attr('data-device');
+    jQuery('.customize-control-kmt-responsive-icon-select .responsive-icon-select, .customize-control .kmt-spacing-responsive-btns > li').removeClass('active');
+    jQuery('.customize-control-kmt-responsive-icon-select .responsive-icon-select.' + device + ', .customize-control .kmt-spacing-responsive-btns > li.' + device).addClass('active');
   });
-  control.container.find(".kmt-responsive-icon-select-btns li button").on("click", function (event) {
+  control.container.find('.kmt-responsive-icon-select-btns li button').on('click', function (event) {
     event.preventDefault();
-    var device = jQuery(this).attr("data-device");
+    var device = jQuery(this).attr('data-device');
 
-    if ("desktop" == device) {
-      device = "tablet";
-    } else if ("tablet" == device) {
-      device = "mobile";
+    if ('desktop' == device) {
+      device = 'tablet';
+    } else if ('tablet' == device) {
+      device = 'mobile';
     } else {
-      device = "desktop";
+      device = 'desktop';
     }
 
-    jQuery('.wp-full-overlay-footer .devices button[data-device="' + device + '"]').trigger("click");
+    jQuery('.wp-full-overlay-footer .devices button[data-device="' + device + '"]').trigger('click');
   });
 }
 function kemetGetResponsiveColorJs(control, child_control_name) {
-  "use strict";
+  'use strict';
 
-  jQuery("html").addClass("responsive-background-color-ready");
-  var device = jQuery(".wp-full-overlay-footer .devices button.active").attr("data-device");
-  jQuery(".customize-control-kmt-responsive-color .customize-control-content .kmt-color-picker-alpha").removeClass("active");
-  jQuery(".customize-control-kmt-responsive-color .customize-control-content .kmt-color-picker-alpha." + device).addClass("active");
-  jQuery(".customize-control-kmt-responsive-color .kmt-color-responsive-btns li").removeClass("active");
-  jQuery(".customize-control-kmt-responsive-color .kmt-color-responsive-btns li." + device).addClass("active");
-  jQuery(".wp-full-overlay-footer .devices button").on("click", function () {
-    var device = jQuery(this).attr("data-device");
-    jQuery(".customize-control-kmt-responsive-color .customize-control-content .kmt-color-picker-alpha").removeClass("active");
-    jQuery(".customize-control-kmt-responsive-color .customize-control-content .kmt-responsive-color." + device).addClass("active");
-    jQuery(".customize-control-kmt-responsive-color .kmt-color-responsive-btns li").removeClass("active");
-    jQuery(".customize-control-kmt-responsive-color .kmt-color-responsive-btns li." + device).addClass("active");
+  jQuery('html').addClass('responsive-background-color-ready');
+  var device = jQuery('.wp-full-overlay-footer .devices button.active').attr('data-device');
+  jQuery('.customize-control-kmt-responsive-color .customize-control-content .kmt-color-picker-alpha').removeClass('active');
+  jQuery('.customize-control-kmt-responsive-color .customize-control-content .kmt-color-picker-alpha.' + device).addClass('active');
+  jQuery('.customize-control-kmt-responsive-color .kmt-color-responsive-btns li').removeClass('active');
+  jQuery('.customize-control-kmt-responsive-color .kmt-color-responsive-btns li.' + device).addClass('active');
+  jQuery('.wp-full-overlay-footer .devices button').on('click', function () {
+    var device = jQuery(this).attr('data-device');
+    jQuery('.customize-control-kmt-responsive-color .customize-control-content .kmt-color-picker-alpha').removeClass('active');
+    jQuery('.customize-control-kmt-responsive-color .customize-control-content .kmt-responsive-color.' + device).addClass('active');
+    jQuery('.customize-control-kmt-responsive-color .kmt-color-responsive-btns li').removeClass('active');
+    jQuery('.customize-control-kmt-responsive-color .kmt-color-responsive-btns li.' + device).addClass('active');
   });
-  control.container.find(".kmt-color-responsive-btns button").on("click", function (event) {
+  control.container.find('.kmt-color-responsive-btns button').on('click', function (event) {
     event.preventDefault();
-    var device = jQuery(this).attr("data-device");
+    var device = jQuery(this).attr('data-device');
 
-    if ("desktop" == device) {
-      device = "tablet";
-    } else if ("tablet" == device) {
-      device = "mobile";
+    if ('desktop' == device) {
+      device = 'tablet';
+    } else if ('tablet' == device) {
+      device = 'mobile';
     } else {
-      device = "desktop";
+      device = 'desktop';
     }
 
-    jQuery('.wp-full-overlay-footer .devices button[data-device="' + device + '"]').trigger("click");
+    jQuery('.wp-full-overlay-footer .devices button[data-device="' + device + '"]').trigger('click');
   });
 
   if (child_control_name) {
     jQuery(document).mouseup(function (e) {
       var container = jQuery(child_control_name);
-      var resColorWrap = container.find(".customize-control-content"); // If the target of the click isn't the container nor a descendant of the container.
+      var resColorWrap = container.find('.customize-control-content'); // If the target of the click isn't the container nor a descendant of the container.
 
       if (!resColorWrap.is(e.target) && resColorWrap.has(e.target).length === 0) {
-        container.find(".components-button.kemet-color-icon-indicate.open").click();
+        container.find('.components-button.kemet-color-icon-indicate.open').click();
+      }
+    });
+  }
+}
+function kemetGetResponsiveBgJs(control, child_control_name) {
+  'use strict';
+
+  jQuery('html').addClass('responsive-background-img-ready');
+  var device = jQuery('.wp-full-overlay-footer .devices button.active').attr('data-device');
+  jQuery('.customize-control-kmt-responsive-background .customize-control-content .background-container').removeClass('active');
+  jQuery('.customize-control-kmt-responsive-background .customize-control-content .background-container.' + device).addClass('active');
+  jQuery('.customize-control-kmt-responsive-background .kmt-responsive-btns li').removeClass('active');
+  jQuery('.customize-control-kmt-responsive-background .kmt-responsive-btns li.' + device).addClass('active');
+  jQuery('.wp-full-overlay-footer .devices button').on('click', function () {
+    var device = jQuery(this).attr('data-device');
+    jQuery('.customize-control-kmt-responsive-background .customize-control-content .background-container').removeClass('active');
+    jQuery('.customize-control-kmt-responsive-background .customize-control-content .background-container.' + device).addClass('active');
+    jQuery('.customize-control-kmt-responsive-background .kmt-responsive-btns li').removeClass('active');
+    jQuery('.customize-control-kmt-responsive-background .kmt-responsive-btns li.' + device).addClass('active');
+  });
+  control.container.find('.kmt-responsive-btns button').on('click', function (event) {
+    event.preventDefault();
+    var device = jQuery(this).attr('data-device');
+
+    if ('desktop' == device) {
+      device = 'tablet';
+    } else if ('tablet' == device) {
+      device = 'mobile';
+    } else {
+      device = 'desktop';
+    }
+
+    jQuery('.wp-full-overlay-footer .devices button[data-device="' + device + '"]').trigger('click');
+  });
+
+  if (child_control_name) {
+    jQuery(document).mouseup(function (e) {
+      var container = jQuery(child_control_name);
+      var bgWrap = container.find('.background-wrapper'); // If the target of the click isn't the container nor a descendant of the container.
+
+      if (!bgWrap.is(e.target) && bgWrap.has(e.target).length === 0) {
+        container.find('.components-button.kemet-color-icon-indicate.open').click();
+      }
+    });
+  }
+}
+function KemetGetResponsiveColorGroupJs(control, child_control_name) {
+  'use strict';
+
+  var device = jQuery('.wp-full-overlay-footer .devices button.active').attr('data-device');
+  jQuery('.customize-control-kmt-color-group .kmt-field-color-group-wrap .kmt-color-group-responsive-wrap').removeClass('active');
+  jQuery('.customize-control-kmt-color-group .kmt-field-color-group-wrap .kmt-color-group-responsive-wrap.' + device).addClass('active');
+  jQuery('.customize-control-kmt-color-group .kmt-responsive-btns li').removeClass('active');
+  jQuery('.customize-control-kmt-color-group .kmt-responsive-btns li.' + device).addClass('active');
+  jQuery('.wp-full-overlay-footer .devices button').on('click', function () {
+    var device = jQuery(this).attr('data-device');
+    jQuery('.customize-control-kmt-color-group .kmt-field-color-group-wrap .kmt-color-group-responsive-wrap').removeClass('active');
+    jQuery('.customize-control-kmt-color-group .kmt-field-color-group-wrap .kmt-color-group-responsive-wrap.' + device).addClass('active');
+    jQuery('.customize-control-kmt-color-group .kmt-responsive-btns li').removeClass('active');
+    jQuery('.customize-control-kmt-color-group .kmt-responsive-btns li.' + device).addClass('active');
+  });
+  control.container.find('.kmt-responsive-btns button').on('click', function (event) {
+    event.preventDefault();
+    var device = jQuery(this).attr('data-device');
+
+    if ('desktop' == device) {
+      device = 'tablet';
+    } else if ('tablet' == device) {
+      device = 'mobile';
+    } else {
+      device = 'desktop';
+    }
+
+    jQuery('.wp-full-overlay-footer .devices button[data-device="' + device + '"]').trigger('click');
+  });
+
+  if (child_control_name) {
+    jQuery(document).mouseup(function (e) {
+      var container = jQuery(child_control_name);
+      var bgWrap = container.find('.background-wrapper'); // If the target of the click isn't the container nor a descendant of the container.
+
+      if (!bgWrap.is(e.target) && bgWrap.has(e.target).length === 0) {
+        container.find('.components-button.kemet-color-icon-indicate.open').click();
       }
     });
   }
