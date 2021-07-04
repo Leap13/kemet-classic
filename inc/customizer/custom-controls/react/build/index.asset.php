71b1ddbc8eb6f211ccabca8a4bd065b6eb9b1500
salmamohamed74 <salma.mohamed@leap13.com>
2021-07-04 11:40:58 +0200

    add attribute for pattern background

--- inc/customizer/custom-controls/react/build/index.asset.php
+++ inc/customizer/custom-controls/react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '441166b870a94a62700234cf73f3242e');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '126d48463bf14ee737ca2af4f0ccf28a');
