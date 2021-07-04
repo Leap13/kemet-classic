9df39e08d9ee7c9b6943668a0703615f204876c8
salmamohamed74 <salma.mohamed@leap13.com>
2021-07-04 16:59:41 +0200

    edit css for color component

--- inc/customizer/custom-controls/react/build/index.asset.php
+++ inc/customizer/custom-controls/react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '8e8db11f8801ee2ea29e43a17cc847be');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '432f2662ea38d54d75d18a895b26c97a');
