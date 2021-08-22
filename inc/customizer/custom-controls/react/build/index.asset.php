4211ec36a940da3f07f5a6a01d39d4fc0be71e0a
salmamohamed74 <sos.mohamed93@yahoo.com>
2021-08-22 23:40:35 +0200

    edit style

--- inc/customizer/custom-controls/react/build/index.asset.php
+++ inc/customizer/custom-controls/react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '4e63da96ca3998db4f7306d217313ae7');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'daa9f2ca827639239f02a397c276225d');
--- inc/customizer/custom-controls/react/src/kmt-controls/color.js
+++ inc/customizer/custom-controls/react/src/kmt-controls/color.js
@@ -1,70 +1,58 @@
-import {
-    createElement,
-    Component,
-    Fragment,
-    createContext,
-    useRef,
-    useContext,
-    useState,
-} from '@wordpress/element'
-import SinglePicker from './color-picker/single-picker'
-import OutsideClickHandler from '../common/outside-component'
+import { useRef, useState } from "@wordpress/element";
+import SinglePicker from "./color-picker/single-picker";
+import OutsideClickHandler from "../common/outside-component";
 
 const ColorComponent = ({ picker, onChangeComplete, value }) => {
+  const [{ isPicking, isTransitioning }, setState] = useState({
+    isPicking: null,
+    isTransitioning: null,
+  });
 
-    const [{ isPicking, isTransitioning }, setState] = useState({
-        isPicking: null,
-        isTransitioning: null,
-    })
+  const containerRef = useRef();
+  const modalRef = useRef();
 
-    const containerRef = useRef()
-    const modalRef = useRef()
+  return (
+    <section>
+      <OutsideClickHandler
+        useCapture={false}
+        display="inline-block"
+        disabled={!isPicking}
+        wrapperProps={{
+          ref: containerRef,
+        }}
+        additionalRefs={[modalRef]}
+        onOutsideClick={() => {
+          setState(({ isPicking }) => ({
+            isPicking: null,
+            isTransitioning: isPicking,
+          }));
+        }}
+      >
+        <SinglePicker
+          containerRef={containerRef}
+          picker={picker}
+          key={picker.id}
+          isPicking={isPicking}
+          modalRef={modalRef}
+          isTransitioning={isTransitioning}
+          onPickingChange={(isPicking) =>
+            setState({
+              isTransitioning: picker.id,
+              isPicking,
+            })
+          }
+          stopTransitioning={() =>
+            setState((state) => ({
+              ...state,
+              isTransitioning: false,
+            }))
+          }
+          onChange={(color) => onChangeComplete(color)}
+          value={value[picker.id]}
+        />
+      </OutsideClickHandler>
+    </section>
+  );
+};
 
-    return (
-        <section>
-            <OutsideClickHandler
-                useCapture={false}
-                display="inline-block"
-                disabled={!isPicking}
-                wrapperProps={{
-                    ref: containerRef,
-                }}
-
-                additionalRefs={[modalRef]}
-                onOutsideClick={() => {
-                    setState(({ isPicking }) => ({
-                        isPicking: null,
-                        isTransitioning: isPicking,
-                    }))
-                }}>
-
-                <SinglePicker
-                    containerRef={containerRef}
-                    picker={picker}
-                    key={picker.id}
-                    isPicking={isPicking}
-                    modalRef={modalRef}
-                    isTransitioning={isTransitioning}
-                    onPickingChange={(isPicking) =>
-                        setState({
-                            isTransitioning: picker.id,
-                            isPicking,
-                        })
-                    }
-                    stopTransitioning={() =>
-                        setState((state) => ({
-                            ...state,
-                            isTransitioning: false,
-                        }))
-                    }
-                    onChange={(color) => onChangeComplete(color)}
-                    value={value[picker.id]}
-                />
-
-            </OutsideClickHandler>
-        </section>
-
-    )
-}
-
-export default ColorComponent
+export default ColorComponent;
