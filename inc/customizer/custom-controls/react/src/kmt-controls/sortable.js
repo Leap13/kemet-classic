1f0d0a5ac2149c41cb5448b53947eba039fd4eab
salmamohamed74 <salma.mohamed@leap13.com>
2021-09-01 17:11:53 +0200

    edit in sortable control

--- inc/customizer/custom-controls/react/src/kmt-controls/sortable.js
+++ inc/customizer/custom-controls/react/src/kmt-controls/sortable.js
@@ -1,6 +1,40 @@
 import PropTypes from 'prop-types';
-import { useRef, useEffect, useState } from 'react';
-import { ReactSortable } from "react-sortablejs";
+import { useState } from 'react';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import { arrayMoveImmutable } from 'array-move';
+
+const SortableItem = SortableElement(({ value, handleClick, newIndex, values }) => {
+
+    if (values.includes(newIndex)) {
+
+        return (
+            <li key={value} className='kmt-sortable-item' data-value={newIndex} >
+
+                <i className="dashicons dashicons-visibility visibility" onClick={() => { handleClick(value, newIndex) }}></i>
+                {value}
+                <i class="dashicons dashicons-menu"></i>
+            </li>
+        )
+    } else {
+        return (<li key={value} className='kmt-sortable-item invisible' data-value={newIndex}>
+
+            <i className="dashicons dashicons-visibility visibility" onClick={() => { handleClick(value, newIndex) }}></i>
+            {value}
+            <i class="dashicons dashicons-menu"></i>
+        </li>)
+    }
+});
+
+
+const SortableList = SortableContainer(({ items, onChange, values }) => {
+    return (
+        <ul>
+            {Object.values(items).map((value, index) => (
+                <SortableItem key={`item-${value[0]}`} distance={10} index={index} newIndex={value[0]} value={value[1]} handleClick={onChange} values={values} />
+            ))}
+        </ul>
+    )
+});
 
 const SortableComponent = props => {
 
@@ -14,10 +48,9 @@ const SortableComponent = props => {
         choices,
         inputAttrs
     } = props.params;
+    const [value, setValue] = useState(props.value);
 
-    const [value, setValue] = useState([choices])
-
-    console.log(value)
+    const [sortItems, setSortItems] = useState(Object.entries(choices))
 
     if (label) {
         labelHtml = <span className="customize-control-title">{label}</span>;
@@ -26,25 +59,39 @@ const SortableComponent = props => {
     if (description) {
         descriptionHtml = <span className="description customize-control-description">{description}</span>;
     }
+    const onSortEnd = ({ oldIndex, newIndex }) => {
+        setSortItems(arrayMoveImmutable(sortItems, oldIndex, newIndex));
+    };
+    const updateValues = (val, thisIndex) => {
+        console.log("HEllo")
+        let newValue = [];
 
-    console.log(props.params, props.value)
+        if (value.includes(thisIndex)) {
+            newValue = value.filter((item) => {
+                return item !== thisIndex
+            })
+
+        } else {
+
+            newValue = [...value, thisIndex]
+        }
+        setValue(newValue)
+        props.onChange(newValue)
+    }
+
+
 
     return <label className='kmt-sortable'>
         {labelHtml}
         {descriptionHtml}
-        <ReactSortable animation={100} className="sortable" list={value} setList={(newValue) => setValue(newValue)}>
-            {value.map((item, index) => {
-                return (
-                    <div
-                        key={item}
-                        style={{ border: "1px solid black", padding: "20px 20px" }}
-                        data-item={item}
-                    >
-                        { item['author']}
-                    </div>
-                );
-            })}
-        </ReactSortable>
+        <SortableList
+            items={sortItems}
+            values={value}
+            onSortEnd={onSortEnd}
+            onChange={(item, index) => updateValues(item, index)}
+            distance={1}
+            lockAxis="y"
+        />
     </label>;
 
 };
