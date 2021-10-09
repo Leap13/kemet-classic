d5f3ac2198a26f2400e75c22f126f06936039122
salmamohamed74 <sos.mohamed93@yahoo.com>
2021-10-09 23:16:40 +0200

    fix color palette comments

--- inc/react-options/src/kmt-controls/color-palettes/AddPaletteContainer.js
+++ inc/react-options/src/kmt-controls/color-palettes/AddPaletteContainer.js
@@ -21,11 +21,13 @@ export default function AddPaletteContainer({
       {...wrapperProps}
     >
       <div className={`kmt-add-palette-container`}>
-        <button className={`button-close-modal`} onClick={(e) => {
-          e.preventDefault();
-          handleCloseModal()
-
-        }}></button>
+        <button
+          className={`button-close-modal`}
+          onClick={(e) => {
+            e.preventDefault();
+            handleCloseModal();
+          }}
+        ></button>
         <div className={`kmt-palette-info`}>
           <div className={`kmt-palette-name`}>
             <p className={`kmt-palette-type-label`}>{__("palette name")}</p>
@@ -33,14 +35,33 @@ export default function AddPaletteContainer({
               type="text"
               className={`kmt-add-palette-title`}
               placeholder={__("name your palette")}
-              onChange={(e) => setPaletteData({ ...data, name: e.target.value })}
+              onChange={(e) =>
+                setPaletteData({ ...data, name: e.target.value })
+              }
             />
           </div>
           <div className={`kmt-Palette-type-container`}>
             <p className={`kmt-palette-type-label`}>{__("palette type")}</p>
             <div className={`kmt-Palette-type-wrapper`}>
-              <span><input type="radio" name="type" onChange={() => setPaletteData({ ...data, type: 'light' })} />Light</span>
-              <span> <input type="radio" name="type" onChange={() => setPaletteData({ ...data, type: 'dark' })} />Dark </span>
+              <span>
+                <input
+                  type="radio"
+                  checked={data.type === "light"}
+                  name="type"
+                  onChange={() => setPaletteData({ ...data, type: "light" })}
+                />
+                Light
+              </span>
+              <span>
+                {" "}
+                <input
+                  type="radio"
+                  checked={data.type === "dark"}
+                  name="type"
+                  onChange={() => setPaletteData({ ...data, type: "dark" })}
+                />
+                Dark{" "}
+              </span>
             </div>
           </div>
           <button
@@ -53,7 +74,7 @@ export default function AddPaletteContainer({
             }}
           >
             Save Palette
-      </button>
+          </button>
         </div>
       </div>
     </animated.div>
