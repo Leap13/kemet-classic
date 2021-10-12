7fdca020f6c7559254b7d981e836285a4cc48d24
salmamohamed74 <salma.mohamed@leap13.com>
2021-10-12 17:36:10 +0200

    apply comments to controls

--- inc/react-options/build/index.asset.php
+++ inc/react-options/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '8f2de0d6f30cbe5e68d9ccd40ac8d19a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'a59f09a2772aaed93ef59e9e70ac01f8');
