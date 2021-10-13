4cc9c788f1e434b74e4bf66d0c0277839cffed51
aaharidy <abdoharidy72@gmail.com>
2021-10-13 17:52:11 +0200

    Merge remote-tracking branch 'origin/v-1.1.21' into v-1.1.19

--- inc/react-options/src/kmt-controls/color-palettes/ColorPalettesModal.js
+++ inc/react-options/src/kmt-controls/color-palettes/ColorPalettesModal.js
@@ -8,46 +8,44 @@ import { useState } from 'react';
 
 const ColorPalettesModal = ({ value, onChange, wrapperProps = {}, handleDeletePalette }) => {
 	const [typeOfPalette, setTypeOfPalette] = useState("light");
-	const paletteColors = value.palettes.filter(palette => { return palette.skin === typeOfPalette })
+	const kemetPaletteColors = value.palettes.filter(palette => { return palette.skin === typeOfPalette && palette.type === "system" })
+	const customPaletteColors = value.palettes.filter(palette => { return palette.skin === typeOfPalette && palette.type === "custom" })
 	return (
 		<animated.div
 			className="kmt-option-modal kmt-palettes-modal"
 			{...wrapperProps}>
 			<div className={`kmt-type-control`}>
-				<span
-					className={classnames({ active: typeOfPalette === "light" })}
+				<span className={classnames({ active: typeOfPalette === "light" })}
 					onClick={() => {
 						setTypeOfPalette("light");
-					}}
-				>
-					Light
-        </span>
-				<span
-					className={classnames({ active: typeOfPalette === "dark" })}
+					}}>Light
+                </span>
+				<span className={classnames({ active: typeOfPalette === "dark" })}
 					onClick={() => {
 						setTypeOfPalette("dark");
 					}}
-				>
-					Dark
-        </span>
+				>	Dark
+                </span>
 			</div>
-			{paletteColors.map((palette, index) => (
+			{ customPaletteColors.length > 0 && <div className={`kmt_label_type__palette`}>{__(`my palettes`)}</div>}
+			{customPaletteColors.map((palette, index) => (
 				<Fragment>
 					<PalettePreview
 						currentPalette={palette}
-						className={
-							value.current_palette === palette.id ? 'kmt-active' : ''
+						className={classnames(`kmt-custom-palette__container`, {
+							'kmt-active': value.current_palette === palette.id
+						})
 						}
 						renderBefore={() => (
 							<Fragment>
 								<label>
 									{palette.name ? palette.name : sprintf(__('Palette #%s', 'Kemet'), index + 1)}
 								</label>
-								{palette[`type`] === "custom" && <button className={`kmt-delete-palette`} onClick={(e) => {
+								<button className={`kmt-delete-palette`} onClick={(e) => {
 									e.preventDefault();
 									e.stopPropagation();
 									handleDeletePalette(palette.id)
-								}}></button>}
+								}}></button>
 							</Fragment>
 						)}
 						onClick={() => {
@@ -63,8 +61,37 @@ const ColorPalettesModal = ({ value, onChange, wrapperProps = {}, handleDeletePa
 				</Fragment>
 
 
-			))}
-		</animated.div>
+			))
+			}
+			<div className={`kmt_label_type__palette`}>{__(`kemet palettes`)}</div>
+			{kemetPaletteColors.map((palette, index) => (
+				<Fragment>
+					<PalettePreview
+						currentPalette={palette}
+						className={
+							value.current_palette === palette.id ? 'kmt-active' : ''
+						}
+						renderBefore={() => (
+							<label>
+								{palette.name ? palette.name : sprintf(__('Palette #%s', 'Kemet'), index + 1)}
+							</label>
+						)}
+						onClick={() => {
+							const { id, ...colors } = palette
+							onChange({
+								...value,
+								current_palette: id,
+								...colors,
+							})
+						}}
+						skipModal={true}
+					/>
+				</Fragment>
+
+
+			))
+			}
+		</animated.div >
 	)
 }
 
