316bdb170d66fb515ec1541ac848743e643f10b5
salmamohamed74 <salma.mohamed@leap13.com>
2021-10-17 17:17:51 +0200

    apply changes to color-palette control

--- inc/react-options/build/index.asset.php
+++ inc/react-options/build/index.asset.php
@@ -1,5 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill' ),
-	'version'      => '3b170ba9dec79fbe7cb63583267329e1',
-);
-
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '935e1becad8d5bbcea8cceca7eba606e');
