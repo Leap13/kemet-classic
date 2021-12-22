53c12a1df24bc6bf95cd2a3c6fde66ef3984f4e0
Nermin <49684695+nerminleap13@users.noreply.github.com>
2021-12-22 11:34:52 +0200

    box-shadow-control-react

--- inc/react-options/build/index.asset.php
+++ inc/react-options/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '4e3d651bfc8064003e12fab1dec1aaa5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '80f24b07330abcad970cc187769f23aa');
--- inc/react-options/src/kmt-controls/option-component.js
+++ inc/react-options/src/kmt-controls/option-component.js
@@ -20,6 +20,7 @@ import EditSettings from "./uploadFont";
 import Border from "./border";
 import ReadymadeHeaders from './readymade-headers'
 import TabsComponent from './tabs'
+import BoxShadow from './box-shadow';
 
 const OptionComponent = (type) => {
     let OptionComponent;
@@ -89,6 +90,9 @@ const OptionComponent = (type) => {
             break;
         case "kmt-readymade-headers":
             OptionComponent = ReadymadeHeaders;
+            break;
+        case "kmt-box-shadow":
+            OptionComponent = BoxShadow;
             break;
     }
 
