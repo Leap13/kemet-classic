1732a1c5d2fb1ff7931ae597cb3db98dec082e29
aaharidy <abdoharidy72@gmail.com>
2022-01-19 20:16:23 +0200

    support tab

--- inc/kemet-panel/assets/js/build/index.asset.php
+++ inc/kemet-panel/assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0da5ba7483843c5ecd12a4b447df69bf');
+<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6b17fbd139ce1a20d66ac6ccb8c4f915');
